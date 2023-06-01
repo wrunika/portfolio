36e2cdccb49e89f6daaa0174006c9fc9176f3343
wrunika <2805nika@gmail.com>
2023-06-01 21:51:09 +0300

    refactor: change component Contacts and styles

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import s from './Contacts.module.css';
 import styleContainer from './../../common/styles/Container.module.css';
+import {Title} from "../../common/components/Title/Title";
+import phoneImg from '../../assets/images/vertical-tablet.svg';
+import mapImg from '../../assets/images/map.svg';
+import emailImg from '../../assets/images/envelope-open.svg';
+import {ContactInfo} from "./ContactInfo/ContactInfo";
 
 export const Contacts = () => {
+    const contactInfo = [
+        {src: mapImg, alt: 'location', href: 'https://www.google.by/maps/place/Minsk', description: 'Minsk, Belarus'},
+        {src: emailImg, alt: 'email', href: 'mailto:2805nika@gmail.com', description: '2805nika@gmail.com'},
+        {src: phoneImg, alt: 'phone', href: 'tel:+375297897245', description: '+375-29-789-72-45'},
+    ]
     return (
         <div className={s.contactsBlock}>
-            <div className={`${styleContainer.container} ${s.contactsContainer}`}>
-                <h2 className={s.title}>Contacts</h2>
-                <form action="" className={s.contactsForm}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea rows={8}></textarea>
-                </form>
-                <button className={s.contactsButton}>Send</button>
+            <div className={styleContainer.container}>
+                <Title title={'Get in touch'}/>
+                <div className={s.contactsContainer}>
+                    <div className={s.contactsInfo}>
+                        {contactInfo.map( (item, index) => {
+                            return(
+                                <ContactInfo
+                                    key={index}
+                                    src={item.src}
+                                    alt={item.alt}
+                                    href={item.href}
+                                    description={item.description}
+                                />
+                            )
+                        } )}
+                    </div>
+                    <form action="" className={s.contactsForm}>
+                        <input placeholder={"Name*"} type="text"/>
+                        <input placeholder={"Email*"} type="text"/>
+                        <textarea placeholder={"Your Message*"} rows={8}></textarea>
+                        <button type={"submit"} className={s.contactsButton}>Send message</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
