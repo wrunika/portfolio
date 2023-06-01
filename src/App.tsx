1ade64f3e7fb4f51c4c4b72031867e7305d629de
wrunika <2805nika@gmail.com>
2023-06-01 21:47:15 +0300

    feat: add new components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
+import {Header} from "./common/components/Header/Header";
 import {Main} from "./components/Main/Main";
 import {Skills} from "./components/Skills/Skills";
 import {Projects} from "./components/Projects/Projects";
 import {Contacts} from "./components/Contacts/Contacts";
 import {Footer} from "./components/Footer/Footer";
+import {AboutMe} from "./components/AboutMe/AboutMe";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <AboutMe />
       <Skills />
       <Projects />
       <Contacts />
