583267bcdec74605dccee2beec12058f9d5b5d61
wrunika <2805nika@gmail.com>
2023-06-01 21:55:49 +0300

    feat: change component Main, add photo, change styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import s from './Main.module.css';
-import styleContainer from './../../common/styles/Container.module.css'
+import styleContainer from './../../common/styles/Container.module.css';
+import myPhoto from './../../assets/images/my-photo.jpg';
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
             <div className={`${styleContainer.container} ${s.mainContainer}`}>
                 <div className={s.mainText}>
-                    <span>Hi there</span>
-                    <h1>I am Veranika Vitukevich</h1>
-                    <p>Frontend developer</p>
+                    <span>Hi there, My name is</span>
+                    <h1>Veranika Vitukevich</h1>
+                    <p className={s.profession}>Frontend <span className={s.accentSpan}>developer</span></p>
                 </div>
-                <div className={s.mainPhoto}></div>
+                <div className={s.mainPhoto}><img src={myPhoto} alt={'this is Me'}/></div>
             </div>
         </div>
     );
