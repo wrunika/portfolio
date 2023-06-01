3a6462d889b291851cc53c237900ea7e40ed300b
wrunika <2805nika@gmail.com>
2023-06-01 21:53:21 +0300

    refactor: change component Footer and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import s from './Footer.module.css';
 import styleContainer from './../../common/styles/Container.module.css';
+import github from '../../assets/images/github-fill.svg';
+import linkedin from '../../assets/images/linkedin-fill.svg';
+import telegram from '../../assets/images/telegram-fill.svg';
+import discord from '../../assets/images/discord-fill.svg';
 
 export const Footer = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footerContainer}`}>
-                <h2 className={s.title}>Veranika Vitukevich</h2>
                 <div className={s.socialIcons}>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
+                    <div className={s.icon}>
+                        <a href="https://github.com/wrunika"><img src={github} alt={'github'}/></a>
+                    </div>
+                    <div className={s.icon}>
+                        <a href="https://github.com/wrunika"><img src={linkedin} alt={'linkedin'}/></a>
+                    </div>
+                    <div className={s.icon}>
+                        <a href="https://github.com/wrunika"><img src={telegram} alt={'telegram'}/></a>
+                    </div>
+                    <div className={s.icon}>
+                        <a href="https://github.com/wrunika"><img src={discord} alt={'discord'}/></a>
+                    </div>
                 </div>
-                <div>2023 All rights reserved</div>
+                <div className={s.footerInfo}>Copyright © 2023 <span>V/Vitukevich</span>. All Rights Reserved.</div>
             </div>
         </div>
     );
