5525278fe3f355d71f371df56e554bb58d6001c2
wrunika <2805nika@gmail.com>
2023-06-01 19:35:59 +0300

    feat: add styles for Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Header.module.css';
-import {Nav} from "../Nav/Nav";
+import {Nav} from "../../../components/Nav/Nav";
 
 export const Header = () => {
     return (
