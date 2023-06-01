826b69bc0c58ed1492ae6bb657a6a0948ebf710a
wrunika <2805nika@gmail.com>
2023-06-01 21:58:33 +0300

    feat: change components Projects and Project, add icons and styles

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -5,12 +5,13 @@ import s from './Project.module.css'
 type PropsType = {
     title: string
     description: string
+    style?: {backgroundImage: string}
 }
-export const Project: React.FC<PropsType> = ({title, description}) => {
+export const Project = ({title, description, style}: PropsType): JSX.Element => {
     return (
         <div className={s.project}>
-            <div className={s.iconContainer}>
-                <a href="">Watch</a>
+            <div className={s.iconContainer} style={style}>
+                {/*<a href="">Watch</a>*/}
             </div>
             <h3 className={s.title}>{title}</h3>
             <span className={s.description}>{description}</span>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import s from './Projects.module.css';
 import styleContainer from './../../common/styles/Container.module.css';
 import {Project} from "./Project/Project";
+import {Title} from "../../common/components/Title/Title";
+import socialImg from "./../../assets/images/social.svg";
+import todoImg from "./../../assets/images/checklist.svg";
+import countImg from "./../../assets/images/count-plus.svg";
+
 
 export const Projects = () => {
+    const socialStyle = {
+        backgroundImage: `url(${socialImg})`,
+    };
+    const todoStyle = {
+        backgroundImage: `url(${todoImg})`,
+    };
+    const counterStyle = {
+        backgroundImage: `url(${countImg})`,
+    };
     return (
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
-                <h2 className={s.title}>Projects</h2>
+                <Title title={'Projects'} />
                 <div className={s.projects}>
-                    <Project title={'Social network'} description={'some description'} />
-                    <Project title={'TODOLIST'} description={'some description'} />
-                    <Project title={'COUNTER'} description={'some description'} />
+                    <Project style={socialStyle} title={'Social network'} description={'some description'} />
+                    <Project style={todoStyle} title={'TODOLIST'} description={'some description'} />
+                    <Project style={counterStyle} title={'Counter'} description={'some description'} />
                 </div>
 
             </div>
