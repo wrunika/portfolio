58f3ad758d671d7503fa38ebbc34a4c33a155bd5
wrunika <2805nika@gmail.com>
2023-06-01 22:16:01 +0300

    feat: change component Nav and styles, add burger

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import s from './Nav.module.css';
+import skillsImage from './../../assets/images/list.svg';
+import projectsImage from './../../assets/images/computer.svg';
+import contactsImage from './../../assets/images/envelope.svg';
+import aboutImage from './../../assets/images/woman.svg';
 
 export const Nav = () => {
     return (
-        <div className={s.nav}>
-            <a href="">Main</a>
-            <a href="">Skills</a>
-            <a href="">Projects</a>
-            <a href="">Contacts</a>
-        </div>
+        <>
+            <div className={s.navGeneral}>
+                <a className={s.main} href="">V</a>
+                <div className={s.nav}>
+                    <a href=""><img src={aboutImage} alt={""}/><span className={s.imgDescription + " " + s.about}>About</span></a>
+                    <a href=""><img src={skillsImage} alt={""}/><span className={s.imgDescription  + " " + s.skills}>Skills</span></a>
+                    <a href=""><img src={projectsImage} alt={""}/><span className={s.imgDescription  + " " + s.projects}>Projects</span></a>
+                    <a href=""><img src={contactsImage} alt={""}/><span className={s.imgDescription + " " + s.contacts}>Contacts</span></a>
+                </div>
+            </div>
+            <div className={s.burgerMenu}>
+                <span className={s.burger}></span>
+            </div>
+        </>
     );
 };
-
