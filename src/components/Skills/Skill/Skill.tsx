5b2f01cfa05b69d27d06b21a9a97074b49bad13a
wrunika <2805nika@gmail.com>
2023-06-01 22:09:05 +0300

    refactor: change components Skill and Skills, and styles

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -3,14 +3,14 @@ import s from './Skill.module.css'
 
 type PropsType = {
     title: string
-    description: string
+    description?: string
 }
 export const Skill: React.FC<PropsType> = ({title, description}) => {
     return (
         <div className={s.skill}>
             <div className={s.icon}></div>
-            <h3 className={s.title}>{title}</h3>
-            <span className={s.description}>{description}</span>
+            <h4 className={s.title}>{title}</h4>
+            {/*<span className={s.description}>{description}</span>*/}
         </div>
     );
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import s from './Skills.module.css';
 import styleContainer from './../../common/styles/Container.module.css'
 import {Skill} from "./Skill/Skill";
+import {Title} from "../../common/components/Title/Title";
 
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
-                <h2 className={s.title}>Skills</h2>
+                <Title title={'Skills'} />
                 <div className={s.skills}>
-                    <Skill title={'HTML'} description={'some description some description some description some description some description'} />
+                    {/*<Skill title={'HTML'} description={'some description some description some description some description some description'} />
                     <Skill title={'CSS 5'} description={'some description'} />
                     <Skill title={'JS'} description={'some description'} />
                     <Skill title={'React'} description={'some description'} />
-                    <Skill title={'Redux'} description={'some description'} />
+                    <Skill title={'Redux'} description={'some description'} />*/}
+                    <Skill title={'HTML'} />
+                    <Skill title={'CSS 5'} />
+                    <Skill title={'JS'} />
+                    <Skill title={'TypeScript'} />
+                    <Skill title={'Git'} />
+                    <Skill title={'React'} />
+                    <Skill title={'Redux'} />
+                    <Skill title={'REST API'} />
+                    <Skill title={'Axios'} />
+                    <Skill title={'Formik'} />
+                    <Skill title={'Unit Tests'} />
+                    <Skill title={'Postman'} />
                 </div>
             </div>
         </div>
