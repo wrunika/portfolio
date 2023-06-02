71f52f4e151e763821f32cf24848ed8d4d12b628
wrunika <2805nika@gmail.com>
2023-06-02 10:46:52 +0300

    feat: add sass into project, refactor AboutMe.module into scss

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import s from './AboutMe.module.css';
+import s from './AboutMe.module.scss';
 import styleContainer from "../../common/styles/Container.module.css";
 import aboutPhoto from '../../assets/images/IMG_1534_cut-photo.ru.jpg';
 import {Title} from "../../common/components/Title/Title";
 
 export const AboutMe = () => {
+    const firstInfoDiv: InfoBlockType[] = [
+        {className: s.infoTitle, title: 'Name:', info: 'Veranika Vitukevich'},
+        {className: s.infoTitle, title: 'Birthday:', info: '28th May 1987'},
+        {className: s.infoTitle, title: 'Languages:', info: ''},
+        {className: s.language, title: 'Belarusian, Russian', info: ' - Native'},
+        {className: s.language, title: 'English', info: ' - B1'},
+        {className: s.language, title: 'Polish', info: ' - B1'},
+    ];
+    const secondInfoDiv: InfoBlockType[] = [
+        {className: s.infoTitle, title: 'Address:', info: 'Minsk, Belarus'},
+        {className: s.infoTitle, title: 'Phone:', info: '(+375) 29 789 72 45'},
+        {className: s.infoTitle, title: 'Email:', info: '2805nika@gmail.com'},
+        {className: s.infoTitle, title: 'Study:', info: 'IT-Incubator'},
+        {className: s.infoTitle, title: 'Remote work:', info: 'Available'},
+    ];
     return (
         <div className={s.aboutBlock}>
             <Title title={'About Me'} />
@@ -16,19 +31,14 @@ export const AboutMe = () => {
                     <p>I'm a Front-end Developer. I'm from Minsk. I would like to code and create web elements for amazing people around the world. I like work with new people. New people, new Experiences.</p>
                     <div className={s.personalInfo}>
                         <div>
-                            <p><span className={s.infoTitle}>Name:</span> Veranika Vitukevich</p>
-                            <p><span className={s.infoTitle}>Birthday:</span> 28th May 1987</p>
-                            <p><span className={s.infoTitle}>Languages:</span></p>
-                            <p><span className={s.language}>Belarusian, Russian</span> - Native</p>
-                            <p><span className={s.language}>English</span> - B1</p>
-                            <p><span className={s.language}>Polish</span> - B1</p>
+                            {firstInfoDiv.map((item, index) => {
+                                return <p key={index}><span className={item.className}>{item.title}</span>{item.info}</p>
+                            })}
                         </div>
                         <div>
-                            <p><span className={s.infoTitle}>Address:</span> Minsk, Belarus</p>
-                            <p><span className={s.infoTitle}>Phone:</span> (+375) 29 789 72 45</p>
-                            <p><span className={s.infoTitle}>Email:</span> 2805nika@gmail.com</p>
-                            <p><span className={s.infoTitle}>Study:</span> IT-Incubator</p>
-                            <p><span className={s.infoTitle}>Remote work:</span> Available</p>
+                            {secondInfoDiv.map((item, index) => {
+                                return <p key={index}><span className={item.className}>{item.title}</span>{item.info}</p>
+                            })}
                         </div>
                     </div>
                 </div>
@@ -36,3 +46,9 @@ export const AboutMe = () => {
         </div>
     );
 };
+
+type InfoBlockType = {
+    className: string
+    title: string
+    info: string
+}
