0904a9289bf768c3f576596dd4928c02adbb5c9b
wrunika <2805nika@gmail.com>
2023-06-04 11:34:35 +0300

    feat: add fontawesome and icon for skills

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -3,12 +3,12 @@ import s from './Skill.module.scss'
 
 type PropsType = {
     title: string
-    description?: string
+    icon?: React.ReactNode
 }
-export const Skill: React.FC<PropsType> = ({title, description}) => {
+export const Skill: React.FC<PropsType> = ({title, icon}) => {
     return (
         <div className={s.skill}>
-            <div className={s.icon}></div>
+            <div className={s.icon}>{icon}</div>
             <h4 className={s.title}>{title}</h4>
         </div>
     );
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import s from './Skills.module.scss';
 import styleContainer from '../../common/styles/Container.module.scss'
-import {Skill} from "./Skill/Skill";
-import {Title} from "../../common/components/Title/Title";
+import {Skill} from './Skill/Skill';
+import {Title} from '../../common/components/Title/Title';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { faSquareJs } from '@fortawesome/free-brands-svg-icons';
+import { faGitAlt } from '@fortawesome/free-brands-svg-icons';
+import { faSass } from '@fortawesome/free-brands-svg-icons';
+import { faBugSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Skills = () => {
     return (
@@ -10,17 +19,17 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <Title title={'Skills'} />
                 <div className={s.skills}>
-                    <Skill title={'HTML'} />
-                    <Skill title={'CSS 5'} />
-                    <Skill title={'JS'} />
+                    <Skill title={'HTML'} icon={<FontAwesomeIcon icon={faHtml5} />} />
+                    <Skill title={'CSS'} icon={<FontAwesomeIcon icon={faCss3Alt} />} />
+                    <Skill title={'JS'} icon={<FontAwesomeIcon icon={faSquareJs} />} />
                     <Skill title={'TypeScript'} />
-                    <Skill title={'Git'} />
-                    <Skill title={'React'} />
+                    <Skill title={'Git'} icon={<FontAwesomeIcon icon={faGitAlt} />} />
+                    <Skill title={'React'} icon={<FontAwesomeIcon icon={faReact}/>} />
                     <Skill title={'Redux'} />
                     <Skill title={'REST API'} />
                     <Skill title={'Axios'} />
-                    <Skill title={'Formik'} />
-                    <Skill title={'Unit Tests'} />
+                    <Skill title={'SASS'} icon={<FontAwesomeIcon icon={faSass} />} />
+                    <Skill title={'Unit Tests'} icon={<FontAwesomeIcon icon={faBugSlash} />} />
                     <Skill title={'Postman'} />
                 </div>
             </div>
