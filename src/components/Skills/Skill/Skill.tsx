d24e8eb6e8c9f636db25d5c73a7449c61e862710
wrunika <2805nika@gmail.com>
2023-07-17 19:41:24 +0300

    feat: add icons for Skills

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -3,12 +3,12 @@ import s from './Skill.module.scss'
 
 type PropsType = {
     title: string
-    iconComponent: React.ReactNode
+    iconComponent: React.ReactNode | string
 }
 export const Skill: React.FC<PropsType> = ({title, iconComponent}) => {
     return (
         <div className={s.skill}>
-            <div className={s.icon}>{iconComponent}</div>
+            {typeof iconComponent === 'string' ? <img className={s.icon + ' ' + s.iconImg} src={iconComponent} alt={'icon'} /> : <div className={s.icon}>{iconComponent}</div>}
             <h4 className={s.title}>{title}</h4>
         </div>
     );
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import s from './Skills.module.scss';
 import {Skill} from './Skill/Skill';
 import {Title} from '../../common/components/Title/Title';
+import tsIcon from './../../assets/images/typescript-icon.svg';
+import reduxIcon from './../../assets/images/redux-icon.svg';
+import axiosIcon from './../../assets/images/axios-icon.svg';
+import postmanIcon from './../../assets/images/postman-icon.svg';
+import apiIcon from './../../assets/images/api-icon.svg';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faReact} from '@fortawesome/free-brands-svg-icons';
 import {faHtml5} from '@fortawesome/free-brands-svg-icons';
@@ -11,7 +16,6 @@ import {faGitAlt} from '@fortawesome/free-brands-svg-icons';
 import {faSass} from '@fortawesome/free-brands-svg-icons';
 import {faBugSlash, IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import {Slide} from "react-awesome-reveal";
-import {AttentionSeeker} from "react-awesome-reveal";
 
 
 export const Skills = () => {
@@ -19,15 +23,15 @@ export const Skills = () => {
         {title: 'HTML', icon: faHtml5},
         {title: 'CSS', icon: faCss3Alt},
         {title: 'JS', icon: faSquareJs},
-        {title: 'TS', icon: faBugSlash},
+        {title: 'TS', icon: tsIcon},
         {title: 'Git', icon: faGitAlt},
         {title: 'React', icon: faReact},
-        {title: 'Redux', icon: faBugSlash},
-        {title: 'REST API', icon: faBugSlash},
-        {title: 'Axios', icon: faBugSlash},
+        {title: 'Redux', icon: reduxIcon},
+        {title: 'REST API', icon: apiIcon},
+        {title: 'Axios', icon: axiosIcon},
         {title: 'SASS', icon: faSass},
         {title: 'Unit Tests', icon: faBugSlash},
-        {title: 'Postman', icon: faBugSlash},
+        {title: 'Postman', icon: postmanIcon},
     ];
     return (
         <div id={'skills'} className={s.skillsBlock}>
@@ -37,7 +41,7 @@ export const Skills = () => {
                     <div className={s.skills}>
                         {skillsInfo.map((s, index) => {
                             return <Slide direction={'left'}>
-                                <Skill key={index} title={s.title} iconComponent={<FontAwesomeIcon icon={s.icon}/>}/>
+                                <Skill key={index} title={s.title} iconComponent={ (typeof s.icon === 'string') ? s.icon :  <FontAwesomeIcon icon={s.icon}/>}/>
                             </Slide>
                         })}
                     </div>
@@ -49,5 +53,5 @@ export const Skills = () => {
 
 type SkillsInfoType = {
     title: string
-    icon: IconDefinition
+    icon: IconDefinition | string
 }
