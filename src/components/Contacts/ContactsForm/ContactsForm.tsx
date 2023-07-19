dc223ed6b4f0d2da9c0b072154d22417dbda3564
wrunika <2805nika@gmail.com>
2023-07-19 15:39:32 +0300

    feat: add useForm into ContactsForm and collect form-data

--- src/components/Contacts/ContactsForm/ContactsForm.tsx
+++ src/components/Contacts/ContactsForm/ContactsForm.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import s from './ContactsForm.module.scss';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+
+type InputsType = {
+    name: string
+    email: string
+    message: string
+}
 const ContactsForm = () => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<InputsType>()
+    const onSubmit: SubmitHandler<InputsType> = (data) => console.log(data)
+
     return (
         <>
-            <form action="" className={s.contactsForm}>
-                <input placeholder={"Name*"} type="text"/>
-                <input placeholder={"Email*"} type="text"/>
-                <textarea placeholder={"Your Message*"} rows={8}></textarea>
+            <form onSubmit={handleSubmit(onSubmit)} className={s.contactsForm}>
+                <input placeholder={"Name*"} {...register('name')} type="text"/>
+                <input placeholder={"Email*"} {...register('email')} type="text"/>
+                <textarea placeholder={"Your Message*"} {...register('message')} rows={8}></textarea>
                 <button type={"submit"} className={s.contactsButton}>Send message</button>
             </form>
         </>
